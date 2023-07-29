cfa40a44109b87f5111b74d1db6b7c94e29066de
nedimburo <nedimb9402@gmail.com>
2023-07-29 18:10:32 +0200

    Added hover effect over squares and minor corrections

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function generateGridsSquares(size=16){
         for (let j=1; j<=size; j++){
             let newSquare=document.createElement("div");
             newSquare.setAttribute("id", "square_"+i+"_"+j);
+            newSquare.setAttribute("onmouseover", "editSquare(this)");
             newSquare.classList.add("square");
             newGrid.appendChild(newSquare);
         }
@@ -21,4 +22,8 @@ function inputNewGrid(){
     let newSize=Number(prompt("Enter the size of new grid(max. 100):"));
     (newSize>100) ? alert("The entered number is greater than 100.") :
     (newSize<1) ? alert("The etnered number is lesser than 1.") : generateGridsSquares(newSize);
+}
+
+function editSquare(element){
+    element.classList.add("colored");
 }
