159b0304fe4fd8e46b1e2dc585fcf343bb0b040c
nedimburo <nedimb9402@gmail.com>
2023-07-29 17:21:17 +0200

    Added function for making new grid

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.addEventListener("load", generateGridsSquares());
 
 function generateGridsSquares(size=16){
     let mainContainer=document.getElementById("container");
+    mainContainer.innerHTML="";
     for (let i=1; i<=size; i++){
         let newGrid=document.createElement("div");
         newGrid.setAttribute("id", "grid_"+i);
@@ -14,4 +15,10 @@ function generateGridsSquares(size=16){
         }
         mainContainer.appendChild(newGrid);
     }
+}
+
+function inputNewGrid(){
+    let newSize=Number(prompt("Enter the size of new grid(max. 100):"));
+    (newSize>100) ? alert("The entered number is greater than 100.") :
+    (newSize<1) ? alert("The etnered number is lesser than 1.") : generateGridsSquares(newSize);
 }
