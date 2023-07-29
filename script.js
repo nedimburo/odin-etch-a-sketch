41965a97375aa2a1ece984d534784a81aa2cad9b
nedimburo <nedimb9402@gmail.com>
2023-07-29 17:07:14 +0200

    Updated generate function and layout

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-window.addEventListener("load", generateGridsSquares);
+window.addEventListener("load", generateGridsSquares());
 
-function generateGridsSquares(){
+function generateGridsSquares(size=16){
     let mainContainer=document.getElementById("container");
-    for (let i=1; i<=16; i++){
+    for (let i=1; i<=size; i++){
         let newGrid=document.createElement("div");
         newGrid.setAttribute("id", "grid_"+i);
         newGrid.classList.add("grid");
-        for (let j=1; j<=16; j++){
+        for (let j=1; j<=size; j++){
             let newSquare=document.createElement("div");
             newSquare.setAttribute("id", "square_"+i+"_"+j);
             newSquare.classList.add("square");
